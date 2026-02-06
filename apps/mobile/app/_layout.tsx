41231d7019153694489c428f12d91498e50fb664
enkhmgln <enkhamgalan560@gmail.com>
2026-02-06 11:30:43 +0800

    Refactor mobile app layout: simplify RootLayout by removing legacy components, introduce new Index component for the main screen, and delete unused modal and tab navigation files to streamline the project structure.

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,24 +1,5 @@
 import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import "react-native-reanimated";
-
-import Providers from "@/components/layout/providers";
-
-export const unstable_settings = {
-  anchor: "(tabs)",
-};
 
 export default function RootLayout() {
-  return (
-    <Providers>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="modal"
-          options={{ presentation: "modal", title: "Modal" }}
-        />
-      </Stack>
-      <StatusBar style="auto" />
-    </Providers>
-  );
+  return <Stack />;
 }
