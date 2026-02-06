b77f23611b26a97f4f0b293fcb0f0a78485a5257
enkhmgln <enkhamgalan560@gmail.com>
2026-02-06 11:31:38 +0800

    Integrate Providers component into RootLayout for enhanced state management with React Query and tRPC. Add new Providers component to encapsulate query client and tRPC provider setup.

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,5 +1,10 @@
 import { Stack } from "expo-router";
+import Providers from "@/components/layout/providers";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Providers>
+      <Stack />
+    </Providers>
+  );
 }
---
+++ apps/mobile/components/layout/providers.tsx
@@ -0,0 +1,15 @@
+import { PropsWithChildren } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { trpc, trpcClient } from "@/lib/trpc";
+
+const queryClient = new QueryClient();
+
+export default function Providers({ children }: PropsWithChildren) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <trpc.Provider client={trpcClient} queryClient={queryClient}>
+        {children}
+      </trpc.Provider>
+    </QueryClientProvider>
+  );
+}
